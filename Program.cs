39649220a48b5ab1c8e60eb5f4e5c532b2fe0686
chancyFox <61965339+chancyFox@users.noreply.github.com>
2020-03-09 19:43:16 +0300

    Add progman
    ffff

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿#region Fog
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #endregion
+            Console.WriteLine("Утро. Поликлиника. Вы в очереди за справкой.");
+            Console.Write("Казалось бы - что может пойти не так? Но вот вы видите старушек." +
+                "Сколько их?"); 
+
+            int countOldLadies = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Их действительно " + countOldLadies);
+
+            int hourseWaiting = (countOldLadies * 10) / 60;
+            int minutesWaiting = (countOldLadies * 10) % 60;
+
+            Console.WriteLine($"И вы понимаете, что ждать вам осталось {hourseWaiting} часов {minutesWaiting} минут...");
+        }
+    }
+}
